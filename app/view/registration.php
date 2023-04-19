1e3582c5d2de5f001b87ca073a92344800de6030
Dima Yemelianov <emelya0666@gmail.com>
2023-04-19 17:16:44 +0300

    added view registration

--- app/view/registration.php
+++ app/view/registration.php
@@ -1,1 +1,12 @@
 
+
+<?php $this->layout('layout', ['title' => 'Реєстрація'])?>
+<link rel="stylesheet" href="css/formRegistr.css">
+
+<form>
+    <h2>Регистрация</h2>
+    <input type="text" placeholder="Имя">
+    <input type="email" placeholder="Email">
+    <input type="password" placeholder="Пароль">
+    <input type="submit" value="Зарегистрироваться">
+</form>
